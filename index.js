c7376fc99e2a63a4d00b4238f3402fa79d17a337
Lorraine M. Gilmore <gilmore.lorraine.m@gmail.com>
2017-12-28 04:26:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function logWhisper(string) {console.log(string.toLowerCase())}
 
 function sayHiToGrandma(string)
 {
-    if (string.toLowerCase()){return ("I can\'t hear you!")}
+    if (string===string.toLowerCase()){return ("I can\'t hear you!")}
   
     else if (string.toUpperCase()){return ("YES INDEED!")}
     
