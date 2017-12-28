fac90649477a810ad501d2187b2c190298bce3a1
Lorraine M. Gilmore <gilmore.lorraine.m@gmail.com>
2017-12-28 04:27:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function sayHiToGrandma(string)
 {
     if (string===string.toLowerCase()){return ("I can\'t hear you!")}
   
-    else if (string.toUpperCase()){return ("YES INDEED!")}
+    else if (string===string.toUpperCase()){return ("YES INDEED!")}
     
     else if (string==="I love you, Grandma."){return ("I love you, too.")}
 }
