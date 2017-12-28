a0f2eaa5ad00c6b3900291793f143fd8887b8d7f
Lorraine M. Gilmore <gilmore.lorraine.m@gmail.com>
2017-12-28 04:23:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function whisper(string){return string.toLowerCase()}
 function logShout(string){console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
 
-function sayHiToGrandma(string)
-{
+
+{function sayHiToGrandma(string)
     if (string.toLowerCase()){return ("I can\'t hear you!")}
   
     else if (string.toUpperCase()){return ("YES INDEED!")}
